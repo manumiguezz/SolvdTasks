166c0d481af783dccb38a2a918d838b5c38b1e11
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-10-25 11:37:31 -0300

    program finished

--- src/main/java/org/manumiguezz/UserInfo.java
+++ src/main/java/org/manumiguezz/UserInfo.java
@@ -1,22 +1,53 @@
 package org.manumiguezz;
 
+import java.util.Scanner;
+
 public class UserInfo {
     public static void main(String[] parameters) {
 
+//      conditional in case not entered information
         if (parameters.length == 3) {
 
             String firstName = parameters [0];
-            String lastName = parameters[1];
+            String lastName = parameters [1];
             int age = Integer.parseInt(parameters[2]);
 
             printUserInfo(firstName, lastName, age);
 
         } else {
-            System.out.println("User information wasnt received :(");
+            exceptionPrint();
         }
     }
 
     public static void printUserInfo(String firstName, String lastName, int age) {
-        System.out.println("Hi " + firstName + " " + lastName + ", you are " + age + "years old ;)");
+        System.out.println("Hi " + firstName + " " + lastName + ", you are " + age + " years old ;)");
+    }
+
+    public static void exceptionPrint() {
+
+//      initializing scanner to get information
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("_________________________________________");
+
+        System.out.println("Hi! your information wasnt received :(");
+
+        System.out.println("please enter your User info to print");
+
+        System.out.println("_________________________________________");
+
+//      asking for information
+        System.out.print("Your first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Your last name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("_________________________________________");
+
+        printUserInfo(firstName, lastName, age);
     }
 }
