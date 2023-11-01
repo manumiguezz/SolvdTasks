4bfd8868b30430088bddf82863f169dccc2af413
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-11-01 10:30:02 -0300

    task three folder created, computer component changed to abstract

---
+++ src/main/java/org/manumiguezz/taskthree/models/ComputerComponent.java
@@ -0,0 +1,50 @@
+package org.manumiguezz.taskthree.models;
+
+import java.util.Objects;
+
+public abstract class ComputerComponent {
+    protected String name;
+    protected String manufacturer;
+
+    public ComputerComponent(String name, String manufacturer) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public abstract void displayDetails();
+
+    @Override
+    public String toString() {
+        return "ComputerComponent{" +
+                "name='" + name + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ComputerComponent that = (ComputerComponent) o;
+        return Objects.equals(name, that.name) && Objects.equals(manufacturer, that.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, manufacturer);
+    }
+}
---
+++ src/main/java/org/manumiguezz/taskthree/models/Memory.java
@@ -0,0 +1,32 @@
+package org.manumiguezz.taskthree.models;
+
+public class Memory extends ComputerComponent {
+    private int sizeGb;
+    private String memoryType;
+
+    public Memory(String name, String manufacturer, int sizeGb, String memoryType) {
+        super(name, manufacturer);
+        this.sizeGb = sizeGb;
+        this.memoryType = memoryType;
+    }
+
+    public int getSizeGb() {
+        return sizeGb;
+    }
+
+    public void setSizeGb(int sizeGb) {
+        this.sizeGb = sizeGb;
+    }
+
+    public String getMemoryType() {
+        return memoryType;
+    }
+
+    public void setMemoryType(String memoryType) {
+        this.memoryType = memoryType;
+    }
+
+    public void usingMemory () {
+        System.out.println("using the total capacity of " + sizeGb + "gb...");
+    }
+}
