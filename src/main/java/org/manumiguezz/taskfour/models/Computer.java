ed7fe6fca340d5108d7da644f314cf3e4fa0cc68
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-11-05 20:28:45 -0300

    task four package initialized

---
+++ src/main/java/org/manumiguezz/taskfour/models/Computer.java
@@ -0,0 +1,60 @@
+package org.manumiguezz.taskfour.models;
+
+public class Computer {
+    private CPU cpu;
+    private GPU gpu;
+    private Motherboard motherboard;
+    private Memory memory;
+    private Storage storage;
+    private PowerSupply powerSupply;
+    private NetworkAdapter networkAdapter;
+    private CoolingSystem coolingSystem;
+
+    public Computer(CPU cpu, GPU gpu, Motherboard motherboard, Memory memory, Storage storage, PowerSupply powerSupply, NetworkAdapter networkAdapter, CoolingSystem coolingSystem) {
+        this.cpu = cpu;
+        this.gpu = gpu;
+        this.motherboard = motherboard;
+        this.memory = memory;
+        this.storage = storage;
+        this.powerSupply = powerSupply;
+        this.networkAdapter = networkAdapter;
+        this.coolingSystem = coolingSystem;
+    }
+
+    public void playGames (ComputerComponent cpu, ComputerComponent gpu, ComputerComponent cool) {
+        cpu.usingComponent();
+        gpu.usingComponent();
+        cool.usingComponent();
+        System.out.println("playing games...");
+    }
+
+    public void playMusic (ComputerComponent cpu, ComputerComponent memory) {
+        cpu.usingComponent();
+        memory.usingComponent();
+        System.out.println("playing music...");
+    }
+
+    public void code (ComputerComponent memory) {
+        memory.usingComponent();
+        System.out.println("coding...");
+    }
+
+    public void chatting (ComputerComponent networkAdapter) {
+        networkAdapter.usingComponent();
+        System.out.println("chatting");
+    }
+
+    public void seeDetails (ComputerComponent coolingSystem, ComputerComponent networkAdapter, ComputerComponent gpu, ComputerComponent cpu,
+                            ComputerComponent memory, ComputerComponent motherboard, ComputerComponent powerSupply, ComputerComponent storage) {
+        System.out.println("Info about each component:");
+        gpu.displayDetails();
+        cpu.displayDetails();
+        motherboard.displayDetails();
+        memory.displayDetails();
+        storage.displayDetails();
+        powerSupply.displayDetails();
+        coolingSystem.displayDetails();
+        networkAdapter.displayDetails();
+
+    }
+}
---
+++ src/main/java/org/manumiguezz/taskfour/models/Memory.java
@@ -0,0 +1,64 @@
+package org.manumiguezz.taskfour.models;
+
+import java.util.Objects;
+
+public class Memory extends ComputerComponent {
+    protected int sizeGb;
+    protected String memoryType;
+
+    public Memory(String name, String manufacturer, int sizeGb, String memoryType) {
+        super(name, manufacturer);
+        this.sizeGb = sizeGb;
+        this.memoryType = memoryType;
+    }
+
+    public int getSizeGb() {
+        return sizeGb;
+    }
+
+    public void setSizeGb(int sizeGb) {
+        this.sizeGb = sizeGb;
+    }
+
+    public String getMemoryType() {
+        return memoryType;
+    }
+
+    public void setMemoryType(String memoryType) {
+        this.memoryType = memoryType;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Your component " + name + " is manufactured by " + manufacturer);
+    }
+
+    @Override
+    public void usingComponent() {
+        System.out.println("using the total capacity of " + sizeGb + "gb...");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Memory memory = (Memory) o;
+        return sizeGb == memory.sizeGb && Objects.equals(memoryType, memory.memoryType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), sizeGb, memoryType);
+    }
+
+    @Override
+    public String toString() {
+        return "Memory{" +
+                "sizeGb=" + sizeGb +
+                ", memoryType='" + memoryType + '\'' +
+                ", name='" + name + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
+}
