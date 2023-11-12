d727cb122852e33e89df81aff9374cffd057d761
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-11-12 17:35:52 -0300

    network adapter class updated with methods

--- src/main/java/org/manumiguezz/tasksix/collections/NetworkArrayList.java
+++ src/main/java/org/manumiguezz/tasksix/collections/NetworkArrayList.java
@@ -3,7 +3,9 @@ package org.manumiguezz.tasksix.collections;
 import org.manumiguezz.tasksix.models.NetworkAdapter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NetworkArrayList {
     private List<NetworkAdapter> networkAdapters;
@@ -19,4 +21,13 @@ public class NetworkArrayList {
     public void deleteNetworkAdapter(NetworkAdapter networkAdapter) {
         networkAdapters.remove(networkAdapter);
     }
+
+    public Set<NetworkAdapter> getAllNetworkAdapters() {
+        return new HashSet<>(networkAdapters);
+    }
+
+    public void clearNetworkAdapters() {
+        networkAdapters.clear();
+    }
+
 }
