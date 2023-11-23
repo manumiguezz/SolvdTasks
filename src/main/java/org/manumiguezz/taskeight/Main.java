485ea81597733791704a20e795890887d0f5adaa
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-11-22 21:51:19 -0300

    try catch

--- src/main/java/org/manumiguezz/taskeight/Main.java
+++ src/main/java/org/manumiguezz/taskeight/Main.java
@@ -1,4 +1,27 @@
 package org.manumiguezz.taskeight;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 
 public class Main {
+
+    String text;
+    String cleanedText;
+
+    {
+        try {
+            text = FileUtils.readFileToString(new File("input.txt"), StandardCharsets.UTF_8);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
