e945c2e4b7707d8af6690ef1f6844bfb9ec39486
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-11-29 23:46:04 -0300

    supplier added to main

--- src/main/java/org/manumiguezz/tasknine/main.java
+++ src/main/java/org/manumiguezz/tasknine/main.java
@@ -1,8 +1,12 @@
 package org.manumiguezz.tasknine;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class main {
 
     Function<Integer, String> intToString = (num) -> "Number: " + num;
+
+    Supplier<Double> getRandom = () -> Math.random();
+
 }
