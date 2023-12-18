2941f1d4e3961bed4b487b8c14e64468fe92a5c9
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-12-17 21:53:18 -0300

    update statements

--- src/main/java/org/manumiguezz/tasktwelve/sql/statements.sql
+++ src/main/java/org/manumiguezz/tasktwelve/sql/statements.sql
@@ -13,7 +13,10 @@ INSERT INTO GPU (gpu_model, computer_id) VALUES ('AMD Radeon RX 5700', 2);
 INSERT INTO Memory (memory_type, memory_capacity, computer_id) VALUES ('DDR4', 16, 1);
 INSERT INTO Memory (memory_type, memory_capacity, computer_id) VALUES ('DDR4', 32, 2);
 
-INSERT INTO Motherboard (motherboard_model) VALUES ('ASUS ROG Strix Z490-E');
+INSERT INTO Motherboard (motherboard_model) VALUES ('ASUS ROG Strix Z390');
 INSERT INTO Motherboard (motherboard_model) VALUES ('MSI B450 TOMAHAWK');
 
-
+UPDATE GPU SET gpu_model = 'Nvidia GeForce RTX 3080' WHERE computer_id = 1;
+UPDATE CPU SET cpu_model = 'Intel Core i7' WHERE cpu_id = 1;
+UPDATE Memory SET memory_capacity = 64 WHERE memory_id = 2;
+UPDATE Motherboard SET motherboard_model = 'ASUS ROG Strix Z590-E' WHERE motherboard_id = 1;
