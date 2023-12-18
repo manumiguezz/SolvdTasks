d744fe04a06b2122a50eb62cf4d659c8eb279e94
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-12-17 21:50:27 -0300

    insertions for cpu gpu and memory

--- src/main/java/org/manumiguezz/tasktwelve/sql/statements.sql
+++ src/main/java/org/manumiguezz/tasktwelve/sql/statements.sql
@@ -3,3 +3,17 @@ INSERT INTO Connection (connection_type) VALUES ('Wi-Fi');
 
 INSERT INTO CoolingSystem (cooling_system_type) VALUES ('Air Cooling');
 INSERT INTO CoolingSystem (cooling_system_type) VALUES ('Liquid Cooling');
+
+INSERT INTO CPU (cpu_model, computer_id) VALUES ('Intel Core i5', 1);
+INSERT INTO CPU (cpu_model, computer_id) VALUES ('AMD Ryzen 7', 2);
+
+INSERT INTO GPU (gpu_model, computer_id) VALUES ('Nvidia GeForce GTX 1660', 1);
+INSERT INTO GPU (gpu_model, computer_id) VALUES ('AMD Radeon RX 5700', 2);
+
+INSERT INTO Memory (memory_type, memory_capacity, computer_id) VALUES ('DDR4', 16, 1);
+INSERT INTO Memory (memory_type, memory_capacity, computer_id) VALUES ('DDR4', 32, 2);
+
+INSERT INTO Motherboard (motherboard_model) VALUES ('ASUS ROG Strix Z490-E');
+INSERT INTO Motherboard (motherboard_model) VALUES ('MSI B450 TOMAHAWK');
+
+
