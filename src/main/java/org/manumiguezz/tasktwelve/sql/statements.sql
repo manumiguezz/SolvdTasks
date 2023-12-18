20a0b14479030c47e55e52f626f261fdb2f3467a
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-12-17 21:59:12 -0300

    update and insertion statements

--- src/main/java/org/manumiguezz/tasktwelve/sql/statements.sql
+++ src/main/java/org/manumiguezz/tasktwelve/sql/statements.sql
@@ -16,7 +16,16 @@ INSERT INTO Memory (memory_type, memory_capacity, computer_id) VALUES ('DDR4', 3
 INSERT INTO Motherboard (motherboard_model) VALUES ('ASUS ROG Strix Z390');
 INSERT INTO Motherboard (motherboard_model) VALUES ('MSI B450 TOMAHAWK');
 
+INSERT INTO PowerSupply (power_supply_model) VALUES ('Corsair RM750');
+INSERT INTO PowerSupply (power_supply_model) VALUES ('EVGA Supernova 650 G5');
+
+INSERT INTO Storage (storage_type, storage_capacity, computer_id) VALUES ('SSD', 500, 1);
+INSERT INTO Storage (storage_type, storage_capacity, computer_id) VALUES ('HDD', 1000, 2);
+
 UPDATE GPU SET gpu_model = 'Nvidia GeForce RTX 3080' WHERE computer_id = 1;
+UPDATE GPU SET gpu_model = 'AMD Radeon RX 5700' WHERE computer_id = 2;
 UPDATE CPU SET cpu_model = 'Intel Core i7' WHERE cpu_id = 1;
 UPDATE Memory SET memory_capacity = 64 WHERE memory_id = 2;
 UPDATE Motherboard SET motherboard_model = 'ASUS ROG Strix Z590-E' WHERE motherboard_id = 1;
+UPDATE PowerSupply SET power_supply_model = 'Corsair RM850x' WHERE power_supply_id = 2;
+UPDATE Storage SET storage_capacity = 2000 WHERE storage_id = 2;
