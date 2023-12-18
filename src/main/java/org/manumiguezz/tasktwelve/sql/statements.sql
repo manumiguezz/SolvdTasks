5b9c3260d56ba9d52e46317617f6e9eba568a229
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-12-17 21:48:55 -0300

    insertions for connection, and cooling system

---
+++ src/main/java/org/manumiguezz/tasktwelve/sql/statements.sql
@@ -0,0 +1,5 @@
+INSERT INTO Connection (connection_type) VALUES ('Ethernet');
+INSERT INTO Connection (connection_type) VALUES ('Wi-Fi');
+
+INSERT INTO CoolingSystem (cooling_system_type) VALUES ('Air Cooling');
+INSERT INTO CoolingSystem (cooling_system_type) VALUES ('Liquid Cooling');
