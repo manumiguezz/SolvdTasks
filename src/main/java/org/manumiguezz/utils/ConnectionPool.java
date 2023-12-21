14b09e87bec77a838e05415415fe9e4a0fd4ccb8
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-12-21 04:07:05 -0300

    connection pool

--- src/main/java/org/manumiguezz/utils/ConnectionPool.java
+++ src/main/java/org/manumiguezz/utils/ConnectionPool.java
@@ -8,6 +8,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import static org.apache.logging.log4j.core.net.UrlConnectionFactory.createConnection;
 
 public class ConnectionPool {
     private static final Logger LOGGER = (Logger) LogManager.getLogger(ConnectionPool.class);
@@ -18,10 +21,36 @@ public class ConnectionPool {
     private static final List<Connection> connectionPool = new ArrayList<>();
     private static final List<Connection> usedConnections = new ArrayList<>();
 
+    static {
+        Properties properties = new Properties();
+        try {
+            properties.load(ConnectionPool.class.getResourceAsStream("config.properties"));
+            url = properties.getProperty("db.url");
+            username = properties.getProperty("db.username");
+            password = properties.getProperty("db.password");
 
+            for (int i = 0; i > maxPoolSize; i++) {
+                Connection connection = createConnection();
+                connectionPool.add(connection);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-    public static Connection getConnection() throws SQLException {
+    private static Connection createConnection() throws SQLException {
         return DriverManager.getConnection(url, username, password);
     }
+
+    public synchronized static Connection getConnection() throws SQLException {
+        if (connectionPool.isEmpty()) {
+            throw new SQLException("No available connections");
+        }
+
+        Connection connection = connectionPool.remove(connectionPool.size() - 1);
+        usedConnections.add(connection);
+        return connection;
+    }
+
 
 }
