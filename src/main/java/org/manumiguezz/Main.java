4b2c6b226e34673a972ed1f64092f9cf2b99f208
Manuel Miguez <manuelmiguezlauria@gmail.com>
2024-01-07 21:36:02 -0300

    method to parse

--- src/main/java/org/manumiguezz/Main.java
+++ src/main/java/org/manumiguezz/Main.java
@@ -3,18 +3,33 @@ package org.manumiguezz;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.manumiguezz.models.Computer;
+import org.manumiguezz.models.Motherboard;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.io.StringReader;
 import java.util.Scanner;
 
 public class Main {
     private static final Logger logger = (Logger) LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+    }
 
-        File computerXml = new File ("src/main/resources/xml/computer.xml");
+    public Computer parseXMLStringToComputerObject(String xmlString) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Computer.class, Motherboard.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
 
+            StringReader reader = new StringReader(xmlString);
+            Computer computer = (Computer) unmarshaller.unmarshal(reader);
+
+            return computer;
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
