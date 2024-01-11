5b190d905f5ab928e15e59bd8bbb8076885147cd
Manuel Miguez <manuelmiguezlauria@gmail.com>
2024-01-11 11:20:43 -0300

    logic added for xml stax parser

--- src/main/java/org/manumiguezz/Main.java
+++ src/main/java/org/manumiguezz/Main.java
@@ -9,7 +9,11 @@ import org.manumiguezz.models.Computer;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -23,9 +27,41 @@ public class Main {
 
         System.out.println("name" + computerJaxb.getComputerName());
 
-        String filePath = "src/main/resources/json/computer.json";
+        String jsonFilePath = "src/main/resources/json/computer.json";
         ObjectMapper objectMapper = new ObjectMapper();
-        Computer computerJson = objectMapper.readValue(filePath, Computer.class);
+        Computer computerJson = objectMapper.readValue(jsonFilePath, Computer.class);
 
+        String xmlFilePath = "src/main/resource/xml/computer.xml";
+
+        try {
+            XMLInputFactory factory = XMLInputFactory.newInstance();
+            XMLStreamReader reader = factory.createXMLStreamReader(new FileInputStream(xmlFilePath));
+
+            while (reader.hasNext()) {
+                int event = reader.next();
+
+                switch (event) {
+                    case XMLStreamConstants.START_ELEMENT:
+                        System.out.println("Start Element: " + reader.getLocalName());
+                        break;
+                    case XMLStreamConstants.CHARACTERS:
+                        String text = reader.getText().trim();
+                        if (!text.isEmpty()) {
+                            System.out.println("Text: " + text);
+                        }
+                        break;
+                    case XMLStreamConstants.END_ELEMENT:
+                        System.out.println("End Element: " + reader.getLocalName());
+                        break;
+                }
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
+
