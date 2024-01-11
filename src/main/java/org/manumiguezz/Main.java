a4a3e17d6d0f84e7504dac1c2160271bd409dd86
Manuel Miguez <manuelmiguezlauria@gmail.com>
2024-01-11 11:06:53 -0300

    changes made

--- src/main/java/org/manumiguezz/Main.java
+++ src/main/java/org/manumiguezz/Main.java
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.manumiguezz.models.Computer;
-import org.manumiguezz.models.Motherboard;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.io.StringReader;
 import java.util.Scanner;
 
 public class Main {
@@ -24,7 +22,6 @@ public class Main {
         Computer computerJaxb = (Computer) unmarshaller.unmarshal(new File("src/main/resources/xml/computer.xml"));
 
         System.out.println("name" + computerJaxb.getComputerName());
-
 
         String filePath = "src/main/resources/json/computer.json";
         ObjectMapper objectMapper = new ObjectMapper();
